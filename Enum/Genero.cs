99cfc4925f1b0bb7c18a44320d8da6c0c7a6112e
Vinicius Molina <viniciusmolina@outlook.com.br>
2021-08-26 16:28:46 -0300

    Adicionar arquivos de projeto.

---
+++ Enum/Genero.cs
@@ -0,0 +1,57 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        IDEs = 1,
+
+        Arquitetura_de_Sistemas = 2,
+
+        Gerenciamento_de_Projetos = 3,
+
+        Git = 4,
+
+        HTML_CSS = 5,
+
+        Linux = 6,
+
+        JavaScript = 7,
+
+        TypeScript = 8,
+
+        ReactJS = 9,
+
+        ReactNative = 10,
+
+        Angular = 11,
+
+        Vue_js = 12,
+
+        Python = 13,
+
+        PHP = 14,
+
+        Node_JS = 15,
+
+        Banco_Dados = 16,
+        Java = 17,
+
+        DOTNET = 18,
+
+        Ruby_On_Rails = 19,
+
+        Azure = 20,
+
+        AWS = 21,
+
+        Docker = 22,
+
+        Kotlin = 23,
+
+        Swift = 24,
+
+        Flutter = 25,
+
+        Empreendedorismo = 26
+
+    }
+}
